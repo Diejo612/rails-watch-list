e96ba360f7831afdbb0482c2b93fee2bc9ddf61a
Jean Pier <diegojeanpier.dc.@gmail.com>
2022-01-21 12:59:31 -0500

    listo

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,14 @@
 import Typed from "typed.js";
 
 const loadDynamicBannerText = () => {
-  new Typed("#banner-typed-text", {
-    strings: ["Terror", "Comedy", "Scy-Fy", "Action"],
-    typeSpeed: 50,
-    loop: true,
-  });
+  let span = document.querySelector("#banner-typed-text");
+  if (span) {
+    new Typed("#banner-typed-text", {
+      strings: ["Terror", "Comedy", "Scy-Fy", "Action"],
+      typeSpeed: 50,
+      loop: true,
+    });
+  }
 };
 
 export { loadDynamicBannerText };
